ffb2edbf767138d24404aaf7e6ac8be50597c253
Erick Reyes <reyespoloerick1@gmail.com>
2024-11-07 16:52:50 -0500

    Diseño de Orden de Compra

--- SoftwareMinimarket/FormRequerimientoList.Designer.cs
+++ SoftwareMinimarket/FormRequerimientoList.Designer.cs
@@ -33,9 +33,11 @@
             this.panelTitleBar = new System.Windows.Forms.Panel();
             this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panelContainer = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvReq)).BeginInit();
             this.panelTitleBar.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.panelContainer.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvReq
@@ -43,11 +45,11 @@
             this.dgvReq.AllowUserToAddRows = false;
             this.dgvReq.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvReq.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvReq.Location = new System.Drawing.Point(12, 62);
+            this.dgvReq.Location = new System.Drawing.Point(17, 16);
             this.dgvReq.Name = "dgvReq";
             this.dgvReq.RowHeadersWidth = 51;
             this.dgvReq.RowTemplate.Height = 24;
-            this.dgvReq.Size = new System.Drawing.Size(776, 382);
+            this.dgvReq.Size = new System.Drawing.Size(768, 362);
             this.dgvReq.TabIndex = 36;
             this.dgvReq.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSeleccion_CellClick);
             // 
@@ -61,6 +63,7 @@
             this.panelTitleBar.Name = "panelTitleBar";
             this.panelTitleBar.Size = new System.Drawing.Size(800, 50);
             this.panelTitleBar.TabIndex = 35;
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
             // 
             // btnCerrar
             // 
@@ -71,6 +74,7 @@
             this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnCerrar.TabIndex = 104;
             this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // label1
             // 
@@ -82,21 +86,37 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "REQUERIMIENTOS";
             // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.dgvReq);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 50);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(800, 400);
+            this.panelContainer.TabIndex = 37;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
+            // 
             // FormRequerimientoList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(0)))), ((int)(((byte)(114)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvReq);
+            this.Controls.Add(this.panelContainer);
             this.Controls.Add(this.panelTitleBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FormRequerimientoList";
             this.Text = "FormRequerimientoList";
+            this.Activated += new System.EventHandler(this.FormRequerimientoList_Activated);
+            this.ResizeEnd += new System.EventHandler(this.FormRequerimientoList_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.FormRequerimientoList_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormRequerimientoList_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormRequerimientoList_MouseDown);
             ((System.ComponentModel.ISupportInitialize)(this.dgvReq)).EndInit();
             this.panelTitleBar.ResumeLayout(false);
             this.panelTitleBar.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.panelContainer.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -107,5 +127,6 @@
         private System.Windows.Forms.Panel panelTitleBar;
         private System.Windows.Forms.PictureBox btnCerrar;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelContainer;
     }
 }
