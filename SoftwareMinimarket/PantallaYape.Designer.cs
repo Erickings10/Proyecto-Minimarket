7d22f34464a8887125c9aef71a83bd64e9c624d4
Erick Reyes <reyespoloerick1@gmail.com>
2024-11-18 18:27:53 -0500

    Diseño Finalizado

--- SoftwareMinimarket/PantallaYape.Designer.cs
+++ SoftwareMinimarket/PantallaYape.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PantallaYape));
-            this.btnConfirmarVenta = new SoftwareMinimarket.CustomControls();
             this.txtCliente = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,37 +40,19 @@
             this.label5 = new System.Windows.Forms.Label();
             this.txtCorreo = new System.Windows.Forms.TextBox();
             this.btnConfirmarPago = new SoftwareMinimarket.CustomControls();
+            this.btnConfirmarVenta = new SoftwareMinimarket.CustomControls();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panelContainer.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnConfirmarVenta
-            // 
-            this.btnConfirmarVenta.BackColor = System.Drawing.Color.Transparent;
-            this.btnConfirmarVenta.BackgroundColor = System.Drawing.Color.Transparent;
-            this.btnConfirmarVenta.BorderColor = System.Drawing.Color.Lime;
-            this.btnConfirmarVenta.BorderRadius = 10;
-            this.btnConfirmarVenta.BorderSize = 2;
-            this.btnConfirmarVenta.FlatAppearance.BorderSize = 0;
-            this.btnConfirmarVenta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
-            this.btnConfirmarVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnConfirmarVenta.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConfirmarVenta.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.btnConfirmarVenta.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirmarVenta.Image")));
-            this.btnConfirmarVenta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnConfirmarVenta.Location = new System.Drawing.Point(614, 364);
-            this.btnConfirmarVenta.Name = "btnConfirmarVenta";
-            this.btnConfirmarVenta.Size = new System.Drawing.Size(183, 50);
-            this.btnConfirmarVenta.TabIndex = 116;
-            this.btnConfirmarVenta.Text = "Confirmar Venta";
-            this.btnConfirmarVenta.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnConfirmarVenta.TextColor = System.Drawing.Color.WhiteSmoke;
-            this.btnConfirmarVenta.UseVisualStyleBackColor = false;
-            this.btnConfirmarVenta.Click += new System.EventHandler(this.btnConfirmarVenta_Click);
             // 
             // txtCliente
             // 
-            this.txtCliente.Location = new System.Drawing.Point(152, 35);
+            this.txtCliente.Location = new System.Drawing.Point(206, 83);
             this.txtCliente.Name = "txtCliente";
-            this.txtCliente.Size = new System.Drawing.Size(100, 22);
+            this.txtCliente.Size = new System.Drawing.Size(176, 22);
             this.txtCliente.TabIndex = 117;
             // 
             // label2
@@ -79,46 +60,46 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Location = new System.Drawing.Point(66, 83);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.Size = new System.Drawing.Size(130, 20);
             this.label2.TabIndex = 119;
-            this.label2.Text = "Nombre Cliente";
+            this.label2.Text = "Nombre Cliente:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label1.Location = new System.Drawing.Point(12, 136);
+            this.label1.Location = new System.Drawing.Point(20, 186);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.Size = new System.Drawing.Size(176, 20);
             this.label1.TabIndex = 120;
-            this.label1.Text = "OTP";
+            this.label1.Text = "Codigo de aprobacion:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Location = new System.Drawing.Point(94, 137);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.Size = new System.Drawing.Size(102, 20);
             this.label3.TabIndex = 121;
-            this.label3.Text = "Monto Total";
+            this.label3.Text = "Monto Total:";
             // 
             // txtTotal
             // 
-            this.txtTotal.Location = new System.Drawing.Point(130, 87);
+            this.txtTotal.Location = new System.Drawing.Point(206, 137);
             this.txtTotal.Name = "txtTotal";
-            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.Size = new System.Drawing.Size(176, 22);
             this.txtTotal.TabIndex = 122;
             // 
             // txtOTP
             // 
-            this.txtOTP.Location = new System.Drawing.Point(87, 134);
+            this.txtOTP.Location = new System.Drawing.Point(206, 186);
             this.txtOTP.Name = "txtOTP";
-            this.txtOTP.Size = new System.Drawing.Size(100, 22);
+            this.txtOTP.Size = new System.Drawing.Size(176, 22);
             this.txtOTP.TabIndex = 123;
             // 
             // label4
@@ -126,17 +107,17 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label4.Location = new System.Drawing.Point(12, 188);
+            this.label4.Location = new System.Drawing.Point(81, 238);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.Size = new System.Drawing.Size(115, 20);
             this.label4.TabIndex = 124;
-            this.label4.Text = "Numero Yape";
+            this.label4.Text = "Numero Yape:";
             // 
             // txtNumYape
             // 
-            this.txtNumYape.Location = new System.Drawing.Point(141, 188);
+            this.txtNumYape.Location = new System.Drawing.Point(206, 236);
             this.txtNumYape.Name = "txtNumYape";
-            this.txtNumYape.Size = new System.Drawing.Size(100, 22);
+            this.txtNumYape.Size = new System.Drawing.Size(176, 22);
             this.txtNumYape.TabIndex = 125;
             // 
             // label5
@@ -144,17 +125,17 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.label5.Location = new System.Drawing.Point(12, 239);
+            this.label5.Location = new System.Drawing.Point(131, 291);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 20);
+            this.label5.Size = new System.Drawing.Size(65, 20);
             this.label5.TabIndex = 126;
-            this.label5.Text = "Correo";
+            this.label5.Text = "Correo:";
             // 
             // txtCorreo
             // 
-            this.txtCorreo.Location = new System.Drawing.Point(130, 237);
+            this.txtCorreo.Location = new System.Drawing.Point(206, 289);
             this.txtCorreo.Name = "txtCorreo";
-            this.txtCorreo.Size = new System.Drawing.Size(100, 22);
+            this.txtCorreo.Size = new System.Drawing.Size(176, 22);
             this.txtCorreo.TabIndex = 127;
             // 
             // btnConfirmarPago
@@ -171,9 +152,9 @@
             this.btnConfirmarPago.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btnConfirmarPago.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirmarPago.Image")));
             this.btnConfirmarPago.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnConfirmarPago.Location = new System.Drawing.Point(16, 364);
+            this.btnConfirmarPago.Location = new System.Drawing.Point(206, 336);
             this.btnConfirmarPago.Name = "btnConfirmarPago";
-            this.btnConfirmarPago.Size = new System.Drawing.Size(183, 50);
+            this.btnConfirmarPago.Size = new System.Drawing.Size(176, 50);
             this.btnConfirmarPago.TabIndex = 128;
             this.btnConfirmarPago.Text = "Confirmar Pago";
             this.btnConfirmarPago.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -181,28 +162,93 @@
             this.btnConfirmarPago.UseVisualStyleBackColor = false;
             this.btnConfirmarPago.Click += new System.EventHandler(this.btnConfirmarPago_Click);
             // 
+            // btnConfirmarVenta
+            // 
+            this.btnConfirmarVenta.BackColor = System.Drawing.Color.Transparent;
+            this.btnConfirmarVenta.BackgroundColor = System.Drawing.Color.Transparent;
+            this.btnConfirmarVenta.BorderColor = System.Drawing.Color.Lime;
+            this.btnConfirmarVenta.BorderRadius = 10;
+            this.btnConfirmarVenta.BorderSize = 2;
+            this.btnConfirmarVenta.FlatAppearance.BorderSize = 0;
+            this.btnConfirmarVenta.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightGreen;
+            this.btnConfirmarVenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmarVenta.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmarVenta.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnConfirmarVenta.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirmarVenta.Image")));
+            this.btnConfirmarVenta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConfirmarVenta.Location = new System.Drawing.Point(605, 388);
+            this.btnConfirmarVenta.Name = "btnConfirmarVenta";
+            this.btnConfirmarVenta.Size = new System.Drawing.Size(183, 50);
+            this.btnConfirmarVenta.TabIndex = 116;
+            this.btnConfirmarVenta.Text = "Confirmar Venta";
+            this.btnConfirmarVenta.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConfirmarVenta.TextColor = System.Drawing.Color.WhiteSmoke;
+            this.btnConfirmarVenta.UseVisualStyleBackColor = false;
+            this.btnConfirmarVenta.Click += new System.EventHandler(this.btnConfirmarVenta_Click);
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.panelTitleBar);
+            this.panelContainer.Controls.Add(this.btnConfirmarVenta);
+            this.panelContainer.Controls.Add(this.btnConfirmarPago);
+            this.panelContainer.Controls.Add(this.label2);
+            this.panelContainer.Controls.Add(this.txtCorreo);
+            this.panelContainer.Controls.Add(this.txtCliente);
+            this.panelContainer.Controls.Add(this.label5);
+            this.panelContainer.Controls.Add(this.label1);
+            this.panelContainer.Controls.Add(this.txtNumYape);
+            this.panelContainer.Controls.Add(this.label3);
+            this.panelContainer.Controls.Add(this.label4);
+            this.panelContainer.Controls.Add(this.txtTotal);
+            this.panelContainer.Controls.Add(this.txtOTP);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(800, 450);
+            this.panelContainer.TabIndex = 129;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panelTitleBar.Controls.Add(this.label6);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(800, 50);
+            this.panelTitleBar.TabIndex = 129;
+            this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 7);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(255, 39);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "PAGO CON YAPE";
+            // 
             // PantallaYape
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(0)))), ((int)(((byte)(114)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnConfirmarPago);
-            this.Controls.Add(this.txtCorreo);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtNumYape);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtOTP);
-            this.Controls.Add(this.txtTotal);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtCliente);
-            this.Controls.Add(this.btnConfirmarVenta);
+            this.Controls.Add(this.panelContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "PantallaYape";
             this.Text = "PantallaYape";
+            this.Activated += new System.EventHandler(this.PantallaYape_Activated);
+            this.ResizeEnd += new System.EventHandler(this.PantallaYape_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.PantallaYape_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PantallaYape_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PantallaYape_MouseDown);
+            this.panelContainer.ResumeLayout(false);
+            this.panelContainer.PerformLayout();
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -219,5 +265,8 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtCorreo;
         private CustomControls btnConfirmarPago;
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Label label6;
     }
 }
